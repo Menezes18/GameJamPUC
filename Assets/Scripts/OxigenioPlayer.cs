ab948ab272a8390c596e3fbb4102d87e33dd406e
Menezes18 <gustavo.e.menezes@outlook.com>
2024-07-26 22:17:58 -0300

    Save scene

--- Assets/Scripts/OxigenioPlayer.cs
+++ Assets/Scripts/OxigenioPlayer.cs
@@ -1,8 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class OxigenioPlayer : MonoBehaviour{
-    public float maxOxigenio;
-    public float minOxigenio;
+    public float oxigenio;
+    public float rate = 1.0f;
+    
+    private void Update(){
+        oxigenio -= rate * Time.deltaTime;
+
+        if (oxigenio < 0){
+            oxigenio = 0;
+            Debug.Log("morreu");
+        }
+    }
 }
