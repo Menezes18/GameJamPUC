f451c85db937c8e2bab129e4e0ce32bae7952aa1
marcusbuzette <32130953+marcusbuzette@users.noreply.github.com>
2024-07-27 07:04:41 -0300

    Quest System part 1

---
+++ Assets/Scripts/Quest/NPC.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    public string[] dialog;
+    public string name;
+
+    public virtual void Interact() {
+        
+    }
+
+}
